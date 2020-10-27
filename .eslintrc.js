6535436473e8b72564d1830de0691735b931f622
valentinmeert <valentin.meert@gmail.com>
2020-10-27 18:04:45 +0100

    add linting

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+  extends: ["airbnb-base"],
+  plugins: ["prettier"],
+  settings: {
+    "import/resolver": {
+      alias: {
+        map: [
+          ["~", "."],
+          ["@", "."],
+        ],
+      },
+    },
+  },
+};
